2f6381f154ada00d1fafd9e7b0181a6cc782f0d6
Enikő <kovacseni@gmail.com>
2020-11-01 18:02:32 +0100

    Javaapi_Navigáció

---
+++ src/main/java/javaapi/Javaapi.java
@@ -0,0 +1,32 @@
+package javaapi;
+
+public class Javaapi {
+
+
+    /* nextLine
+    public String nextLine()
+    Advances this scanner past the current line and returns the input that was skipped. This method returns the rest of the current line, excluding any line separator at the end. The position is set to the beginning of the next line.
+
+            Since this method continues to search through the input looking for a line separator, it may buffer all of the input searching for the line to skip if no line separators are present.
+
+            Returns:
+    the line that was skipped
+    Throws:
+    NoSuchElementException - if no line was found
+    IllegalStateException - if this scanner is closed */
+
+    /*
+nextInt
+public int nextInt()
+Scans the next token of the input as an int.
+
+An invocation of this method of the form nextInt() behaves in exactly the same way as the invocation nextInt(radix), where radix is the default radix of this scanner.
+
+Returns:
+    the int scanned from the input
+Throws:
+    InputMismatchException - if the next token does not match the Integer regular expression, or is out of range
+    NoSuchElementException - if input is exhausted
+    IllegalStateException - if this scanner is closed */
+
+}
