5a9e119f41538e9024eb7f848cb4e739736ef420
Enikő <kovacseni@gmail.com>
2020-11-06 16:15:18 +0100

    FinalAttributum

---
+++ src/main/java/finalmodifier/FinalAttributum.java
@@ -0,0 +1,12 @@
+package finalmodifier;
+
+public class FinalAttributum {
+
+    /* final double a; */
+
+    public static void main(String[] args) {
+        final double b;
+        b = 34;
+        System.out.println(b);
+    }
+}
