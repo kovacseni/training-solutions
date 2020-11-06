b42ebe323f052484af78a3ab1f2c34879555ef8a
Enikő <kovacseni@gmail.com>
2020-11-06 13:13:31 +0100

    Languages

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+
+        List<String> programNyelvek = new ArrayList<>();
+        programNyelvek.add("Java");
+        programNyelvek.add("Python");
+        programNyelvek.add("JavaScript");
+
+        for (String otKarakter : programNyelvek) {
+            if (otKarakter.length() > 5) {
+                System.out.println(otKarakter);
+            }
+        }
+    }
+}
