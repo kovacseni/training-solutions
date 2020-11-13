57e0fa63b7cf61c4586f3f20d20f42e9386b0a5a
Enikő <kovacseni@gmail.com>
2020-11-13 18:11:21 +0100

    Game

---
+++ src/main/java/math/game/Warrior.java
@@ -0,0 +1,15 @@
+package math.game;
+
+public class Warrior {
+
+    private String name;
+    private int stamina;
+    private double skill;
+    private Point position;
+
+    public Warrior(String name, Point position) {
+        this.name = name;
+        this.position = position;
+    }
+
+}
