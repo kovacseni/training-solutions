24e8db6198457ee48ebb311c0a743a463065b6cd
Enikő <kovacseni@gmail.com>
2020-11-13 18:12:16 +0100

    Game

--- src/main/java/math/game/Game.java
+++ src/main/java/math/game/Game.java
@@ -2,8 +2,8 @@ package math.game;
 
 public class Game {
 
-    Warrior w1 = new Warrior("Pokemon", new Point (2, 3))
-    Warrior w2 = new Warrior("Dragon", new Point (8, 4))
+    Warrior w1 = new Warrior("Pokemon", new Point (2, 3));
+    Warrior w2 = new Warrior("Dragon", new Point (8, 4));
 }
 
 /* Ezzel a feladat-családdal összesen eddig jutottam, de egyelőre elküldöm, mert bőven van mit csinálni még. :) */
