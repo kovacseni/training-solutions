e5766c7a43510c3f3da29d7c9d7d46aa51e9625f
Enikő <kovacseni@gmail.com>
2020-11-15 13:50:25 +0100

    UnitConverter

--- src/main/java/enumtype/unit/UnitConverter.java
+++ src/main/java/enumtype/unit/UnitConverter.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class UnitConverter {
 
-    public BigDecimal convert(BigDecimal length, LengthUnit source, LengthUnit target) {
+    public
+    BigDecimal convert(BigDecimal length, LengthUnit source, LengthUnit target) {
         BigDecimal big1 = new BigDecimal(source.inMm());
         BigDecimal big2 = new BigDecimal(target.inMm());
         return length.multiply(big1).divide(big2, 4, RoundingMode.HALF_UP);
