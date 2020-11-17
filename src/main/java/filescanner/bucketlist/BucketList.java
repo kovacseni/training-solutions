411ad01e21ee2a7e2b7b611ae841302f56e0d014
Enikő <kovacseni@gmail.com>
2020-11-17 17:38:16 +0100

    FileScanner

---
+++ src/main/java/filescanner/bucketlist/BucketList.java
@@ -0,0 +1,21 @@
+package filescanner.bucketlist;
+
+import java.util.Scanner;
+
+public class BucketList {
+
+    public static void main(String[] args) {
+
+   try (Scanner scanner = new Scanner(BucketList.class.getResourceAsStream("/bakancslista.txt")) {
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+    }
+        }
+}
+
+
+
+
+
