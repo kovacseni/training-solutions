22135b602bafb7a0d1f0b137f910f659180da05d
Enikő <kovacseni@gmail.com>
2020-11-18 14:56:20 +0100

    NumberGuesser

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -14,7 +14,6 @@ public class NumberGuesser {
         System.out.println("Gondoltam egy számra. Találja ki a számot!");
         int a = scanner.nextInt();
 
-
         while (a != r) {
             if (a > r) {
                 System.out.println("Ez a szám nagyobb, mint a gondolt szám.");
@@ -24,11 +23,12 @@ public class NumberGuesser {
                 System.out.println("Ez a szám kisebb, mint a gondolt szám.");
                 System.out.println("Adjon meg egy másik számot!");
                 a = scanner.nextInt();
-            } else if (a == r) {
-                System.out.println("Igen, erre a számra gondoltam!");
-                return;
             }
         }
+
+        if (a == r) {
+        System.out.println("Igen, erre a számra gondoltam!");
+    }
 
     }
 }
