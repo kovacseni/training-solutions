5478865322a2d2320442d5356ad32efab348045c
Enikő <kovacseni@gmail.com>
2020-12-08 16:03:42 +0100

    ClassRoom

---
+++ src/main/java/inheritanceconstructor/classroom/ClassRoom.java
@@ -0,0 +1,23 @@
+package inheritanceconstructor.classroom;
+
+public class ClassRoom extends Room {
+
+    private Facility facility;
+
+    public ClassRoom(String location, int capacity, Facility facility) {
+        super(location, capacity);
+        this.facility = facility;
+    }
+
+    public Facility getFacility() {
+        return facility;
+    }
+
+    public boolean isSuitable(Course course) {
+        boolean suitable = false;
+        if (course.getParticipants() <= this.getCapacity() && course. getFacilityNeeded() == this.facility) {
+            suitable = true;
+        }
+        return suitable;
+    }
+}
