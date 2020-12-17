9216ab1933a544f21c024029844163f92cb1557d
Enikő <kovacseni@gmail.com>
2020-12-17 12:42:34 +0100

    CanMark

---
+++ src/main/java/week08d04/BadMood.java
@@ -0,0 +1,8 @@
+package week08d04;
+
+public class BadMood implements CanMark {
+
+    public int giveMark() {
+        return 3;
+    }
+}
---
+++ src/test/java/week08d04/CanMarkTest.java
@@ -0,0 +1,13 @@
+package week08d04;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CanMarkTest {
+
+    @Test
+    public void trainerTest() {
+        Assertions.assertEquals(5, new Trainer(new GoodMood()).giveMark());
+        Assertions.assertEquals(3, new Trainer(new BadMood()).giveMark());
+    }
+}
