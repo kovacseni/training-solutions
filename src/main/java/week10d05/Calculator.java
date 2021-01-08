d4753a98e45df59f5008867fbd11696a614b44a4
Enikő <kovacseni@gmail.com>
2021-01-08 13:53:05 +0100

    Calculator

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -1,25 +1,22 @@
 package week10d05;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Calculator {
 
     public int findMinSum(int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Adjon meg egy létező tömböt!");
+        }
+
         int minSum = 0;
         if (arr.length <= 4) {
             for (Integer i : arr) {
                 minSum += i;
             }
         } else {
-            List<Integer> integerList = new ArrayList<>();
-            for (Integer i : arr) {
-                integerList.add(i);
-            }
-            Collections.sort(integerList);
-            minSum = integerList.get(0) + integerList.get(1) + integerList.get(2) + integerList.get(3);
+           Arrays.sort(arr);
+            minSum = arr[0] + arr[1] + arr[2] + arr[3];
         }
         return minSum;
     }
--- src/test/java/week10d05/CalculatorTest.java
+++ src/test/java/week10d05/CalculatorTest.java
@@ -9,8 +9,10 @@ public class CalculatorTest {
     public void testFindMinSum() {
         int[] arr1 = {9, 3, 5, 1, 7};
         int[] arr2 = {3, 5, 1};
+        int[] arr3 = {3, 1, 5, 7, 3, 9, 3, 1};
 
         Assertions.assertEquals(16, new Calculator().findMinSum(arr1));
         Assertions.assertEquals(9, new Calculator().findMinSum(arr2));
+        Assertions.assertEquals(8, new Calculator().findMinSum(arr3));
     }
 }
