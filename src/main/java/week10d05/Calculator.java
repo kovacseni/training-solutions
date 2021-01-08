a00885e0e3b2bae26e7cec752964a6a93cfbf099
Enikő <kovacseni@gmail.com>
2021-01-08 14:53:56 +0100

    Calculator

--- src/main/java/week10d05/Calculator.java
+++ src/main/java/week10d05/Calculator.java
@@ -1,10 +1,11 @@
 package week10d05;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Calculator {
 
-    public int findMinSum(int[] arr) {
+    public void findMinSum(int[] arr) {
         if (arr == null) {
             throw new IllegalArgumentException("Adjon meg egy létező tömböt!");
         }
@@ -18,6 +19,17 @@ public class Calculator {
            Arrays.sort(arr);
            minSum = arr[0] + arr[1] + arr[2] + arr[3];
         }
-        return minSum;
+        System.out.println(minSum);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {3, 9, 7, 5, 1};
+        int[] arr2 = {3, 1, 5};
+        int[] arr3 = {3, 1, 9, 7, 3, 1, 9, 7, 3, 5, 7};
+        int[] arr4 = {3, -3, -5, 1, -7, 7, 9};
+        new Calculator().findMinSum(arr1);
+        new Calculator().findMinSum(arr2);
+        new Calculator().findMinSum(arr3);
+        new Calculator().findMinSum(arr4);
     }
 }
