d1c7041f0533ede145c166d6f7f9568e096d4368
Enikő <kovacseni@gmail.com>
2021-01-18 21:49:09 +0100

    StateRegister

---
+++ src/main/java/ioreader/states/StateRegister.java
@@ -0,0 +1,44 @@
+package ioreader.states;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StateRegister {
+
+    private List<State> states = new ArrayList<>();
+
+    public List<State> getStates() {
+        return new ArrayList<>(states);
+    }
+
+    public void readStatesFromFile(String stringFileName) {
+        try {
+            Path path = Path.of("src/main/resources/" + stringFileName);
+            BufferedReader br = Files.newBufferedReader(path);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] splittedLine = line.split("-");
+                states.add(new State(splittedLine[0], splittedLine[1]));
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can't read file!");
+        }
+    }
+
+    public String findCapitalByStateName(String stateName) {
+        String stateCapital = null;
+        for (State s : states) {
+            if (s.getStateName().equals(stateName)) {
+                stateCapital = s.getCapital();
+            }
+        }
+        if (stateCapital == null) {
+            throw new IllegalArgumentException("No state with this name!");
+        }
+        return stateCapital;
+    }
+}
