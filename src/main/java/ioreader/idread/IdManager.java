3f195e4431a9bc6cf81dd49b4f3468a032a4cbe0
Enikő <kovacseni@gmail.com>
2021-01-18 21:16:33 +0100

    IdManager

---
+++ src/main/java/ioreader/idread/IdManager.java
@@ -0,0 +1,31 @@
+package ioreader.idread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdManager {
+
+    private List<String> ids = new ArrayList<>();
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public void readIdsFromFile(String fileName) {
+        try {
+            Path path = Path.of("src/main/resources/" + fileName);
+            BufferedReader br = Files.newBufferedReader(path);
+            String line;
+            while ((line= br.readLine()) != null) {
+                ids.add(line);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file.", ioe);
+        }
+
+    }
+}
