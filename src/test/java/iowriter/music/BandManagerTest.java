7a10cf65aec1eb4ac3fbb9f410a5aedfbf7ed74e
Enikő <kovacseni@gmail.com>
2021-01-19 16:15:13 +0100

    BandManager

--- src/test/java/iowriter/music/BandManagerTest.java
+++ src/test/java/iowriter/music/BandManagerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
