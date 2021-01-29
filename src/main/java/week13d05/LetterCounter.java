3699dc35b4096da3c62023bddfbcd1d5848fdfee
Enikő <kovacseni@gmail.com>
2021-01-29 15:37:31 +0100

    LetterCounter

--- src/main/java/week13d05/LetterCounter.java
+++ src/main/java/week13d05/LetterCounter.java
@@ -1,18 +1,51 @@
 package week13d05;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class LetterCounter {
 
     public int countDifferentLetters(String word) {
        Set<Character> differentLetters = new TreeSet<>();
-       char[] lettersOfWord = word.toCharArray();
+       char[] lettersOfWord = word.toLowerCase().toCharArray();
        for (char c : lettersOfWord) {
            if (c >= 'a' && c <= 'z') {
                differentLetters.add(c);
            }
        }
        return differentLetters.size();
+    }
+
+    public Map<Character, Integer> countLetters(String word) {
+        Map<Character, Integer> lettersOfWord = new TreeMap<>();
+        int[] keyAndValue = new int[2];
+        char[] letters = word.toLowerCase().toCharArray();
+        for (char c : letters) {
+            if (c >= 'a' && c <= 'z') {
+                keyAndValue = getKeyAndValue(c, lettersOfWord);
+                lettersOfWord.put((char)keyAndValue[0], keyAndValue[1]);
+            }
+        }
+        return lettersOfWord;
+    }
+
+    private int[] getKeyAndValue(char c, Map<Character, Integer> lettersOfWord) {
+        int[] keyAndValue = new int[2];
+        if (!lettersOfWord.containsKey(c)) {
+            keyAndValue[0] = c;
+            keyAndValue[1] = 1;
+        }
+        if (lettersOfWord.containsKey(c)) {
+            int sum = lettersOfWord.get(c) + 1;
+            keyAndValue[0] = c;
+            keyAndValue[1] = sum;
+        }
+        return keyAndValue;
+    }
+
+    public static void main(String[] args) {
+        LetterCounter lc = new LetterCounter();
+        lc.countLetters("Almafa-, Levélbogár");
+        System.out.println(lc.countLetters("Almafa-, Levélbogár").keySet().toString());
+        System.out.println(lc.countLetters("Almafa-, Levélbogár").entrySet().toString());
     }
 }
--- src/test/java/week13d05/LetterCounterTest.java
+++ src/test/java/week13d05/LetterCounterTest.java
@@ -10,4 +10,9 @@ public class LetterCounterTest {
         Assertions.assertEquals(6, new LetterCounter().countDifferentLetters("gólkirály"));
         Assertions.assertEquals(17, new LetterCounter().countDifferentLetters("A tájképi rendeletet tegnap 13-an elolvasták, ma 14-en megszavazták."));
     }
+
+    @Test
+    public void testCountLetters() {
+        Assertions.assertEquals("[a=3, b=1, e=1, f=1, g=1, l=3, m=1, o=1, r=1, v=1]", new LetterCounter().countLetters("Almafa-, Levélbogár").entrySet().toString());
+    }
 }
