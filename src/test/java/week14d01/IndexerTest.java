69912b17b11732be31977ca8d5f594233ff726bc
Enikő <kovacseni@gmail.com>
2021-02-01 14:49:08 +0100

    Indexer

--- src/test/java/week14d01/IndexerTest.java
+++ src/test/java/week14d01/IndexerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class IndexerTest {
 
@@ -12,5 +13,12 @@ public class IndexerTest {
     public void testIndex() {
         List<String> nameList = Arrays.asList("Abraham", "Lujza", "Odon", "Magdolna", "Ludmilla", "Laszlo", "Ottokar", "Ajtony");
         Assertions.assertEquals("{A=[Abraham, Ajtony], L=[Lujza, Ludmilla, Laszlo], M=[Magdolna], O=[Odon, Ottokar]}", new Indexer().index(nameList).toString());
+    }
+
+    @Test
+    public void testIndex2() {
+        List<String> nameList = Arrays.asList("Abraham", "Lujza", "Odon", "Magdolna", "Ludmilla", "Laszlo", "Ottokar", "Ajtony");
+        Map<Character, List<String>> expected = Map.of('A', Arrays.asList("Abraham", "Ajtony"), 'L', Arrays.asList("Lujza", "Ludmilla", "Laszlo"), 'M', Arrays.asList("Magdolna"), 'O', Arrays.asList("Odon", "Ottokar"));
+        Assertions.assertEquals(expected, new Indexer().index(nameList));
     }
 }
