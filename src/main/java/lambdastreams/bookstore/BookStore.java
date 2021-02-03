faa149bfc89f4690cb39a9dc28789288d52bd4b3
Enikő <kovacseni@gmail.com>
2021-02-03 11:11:49 +0100

    BookStore

---
+++ src/main/java/lambdastreams/bookstore/BookStore.java
@@ -0,0 +1,32 @@
+package lambdastreams.bookstore;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+public class BookStore {
+
+    private List<Book> books;
+
+    public BookStore(List<Book> books) {
+        this.books =books;
+    }
+
+    public int getNumberOfBooks() {
+        return (int)books.stream().reduce(0, (a, b) -> a + b.getPieces(), (x, y) -> x + y);
+    }
+
+    public Optional<Book> findNewestBook() {
+        return books.stream().max(Comparator.comparingInt(Book::getYearOfPublish));
+    }
+
+    public int getTotalValue() {
+        return books.stream().reduce(0, (a, b) -> a + b.getPrice() * b.getPieces(), (x, y) -> x + y);
+    }
+
+    public List<Book> getByYearOfPublish(int year) {
+        return books.stream().collect(Collectors.groupingBy(Book::getYearOfPublish)).get(year);
+    }
+}
