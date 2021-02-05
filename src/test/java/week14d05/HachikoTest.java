e96fef535737f9650fdeec963e72aeafc7d1f958
Enikő <kovacseni@gmail.com>
2021-02-05 13:41:01 +0100

    Hachiko

--- src/test/java/week14d05/HachikoTest.java
+++ src/test/java/week14d05/HachikoTest.java
@@ -1,10 +1,10 @@
 package week14d05;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.Map;
 
@@ -12,10 +12,11 @@ public class HachikoTest {
 
     @Test
     public void testCountWords() {
-        try (InputStream is = HachikoTest.class.getResourceAsStream("src/main/resources/hachiko.srt");
-             InputStreamReader isr = new InputStreamReader(is, Charset.forName("windows-1250"))) {
-                Map<String, Integer> wordsSum = new Hachiko().countWords(isr, "Hachiko", "haza", "pályaudvar", "jó");
-                System.out.println(wordsSum.toString());
+        try (FileReader fr = new FileReader("src/main/resources/hachiko.srt", Charset.forName("windows-1250"))) {
+                Map<String, Integer> wordsSum = new Hachiko().countWords(fr, "Hachiko", "haza", "pályaudvar", "jó");
+
+            Assertions.assertEquals("{haza=15, Hachiko=4, jó=15, pályaudvar=5}", wordsSum.toString());
+
         } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file.", ioe);
         }
