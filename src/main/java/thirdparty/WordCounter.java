4ce8d065863bd0c46e82e10b26eec1249c1ac4f9
Enikő <kovacseni@gmail.com>
2021-02-14 10:25:24 +0100

    WordCounter

---
+++ src/main/java/thirdparty/WordCounter.java
@@ -0,0 +1,15 @@
+package thirdparty;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+public class WordCounter {
+
+    public int numberOfWords(String s) {
+        return Splitter.on(CharMatcher.anyOf(" ,."))
+                .trimResults()
+                .omitEmptyStrings()
+                .splitToList(s)
+                .size();
+    }
+}
---
+++ src/main/java/thirdparty/WordCounterMain.java
@@ -0,0 +1,12 @@
+package thirdparty;
+
+public class WordCounterMain {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("0");
+        } else {
+            System.out.println(new WordCounter().numberOfWords(args[0]));
+        }
+    }
+}
